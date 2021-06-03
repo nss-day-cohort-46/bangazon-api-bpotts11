373560896eb26f1f875ac7f2429034f4faafae20
bpotts11 <brittneympotts@gmail.com>
2021-06-03 15:14:14 -0500

    feat: report given for unpaid orders

--- bangazonProject.session.sql
+++
@@ -1,5 +0,0 @@
-SELECT *
-FROM bangazonapi_order o
-    JOIN bangazonapi_customer c ON o.customer_id = c.id
-    JOIN bangazonapi_orderproduct op ON o.id = op.order_id
-    JOIN bangazonapi_product p ON p.id = op.product_id
--- bangazonreports/urls.py
+++ bangazonreports/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import completedorder_list
+from .views import completedorder_list, incompleteorder_list
 
 urlpatterns = [
     path('reports/completedorders', completedorder_list),
+    path('reports/incompleteorders', incompleteorder_list),
 ]
